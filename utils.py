3f7a1e46489102770f313da179f6b1720d11007a
JohanLundberg12 <johanlundberg1993@gmail.com>
2022-10-23 17:54:09 +0000

    gitignore and utils

---
+++ utils.py
@@ -0,0 +1,11 @@
+import torch
+
+
+def get_device():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    print(f"Using {device} as backend")
+
+    return device
